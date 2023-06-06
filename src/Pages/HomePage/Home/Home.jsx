fd22aed0fe2c05e02511acecb7530cfe6b1bc485
Doha0 <abu.jardoha@gmail.com>
2023-06-06 23:32:28 +0600

    project setup

---
+++ src/Pages/HomePage/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../../Components/Shared/Header/Header';
+import Footer from '../../../Components/Shared/Footer/Footer';
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
